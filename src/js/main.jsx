40041c2e8cb825836279d40cb3371ba43c911245
Miguel Toyas Pernichi <mitoperni@gmail.com>
2025-02-10 11:11:55 +0000

    Cambiado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "bootstrap";
 
 // index.css'
-import '../styles/index.css'
+import "../styles/index.css";
+
+// ESTO ES UN CAMBIO
 
 // components
-import Home from './components/Home';
+import Home from "./components/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
